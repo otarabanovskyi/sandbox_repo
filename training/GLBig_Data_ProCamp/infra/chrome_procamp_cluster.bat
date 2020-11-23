557e54467bf386375cf067763d600cc5f9a3d544
Oleksandr Tarabanovskyi <altar@diceus.com>
2020-11-23 15:41:19 +0200

    added local env GCP scripts

---
+++ training/GLBig_Data_ProCamp/infra/chrome_procamp_cluster.bat
@@ -0,0 +1,3 @@
+"%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" ^
+  --proxy-server="socks5://localhost:1080" ^
+  --user-data-dir="%Temp%\procamp-cluster-m" http://procamp-cluster-m:8088
---
+++ training/GLBig_Data_ProCamp/infra/create_ssh_tunnel.bat
@@ -0,0 +1,3 @@
+gcloud compute ssh procamp-cluster-m ^
+  --project=bigdata-procamp-35432c21 ^
+  --zone=us-east1-b -- -D 1080 -N
