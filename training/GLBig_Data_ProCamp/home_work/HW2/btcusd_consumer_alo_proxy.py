0e299a1d0cf7925661456c623d533056f0b3d27a
Oleksandr Tarabanovskyi <altar@diceus.com>
2020-11-24 10:15:40 +0200

    try to use proxy for kafka brocker

--- training/GLBig_Data_ProCamp/home_work/HW2/btcusd_consumer_alo_proxy.py
+++ training/GLBig_Data_ProCamp/home_work/HW2/btcusd_consumer_alo_proxy.py
@@ -2,7 +2,23 @@
 #python-kafka sjould be installed
 #If kafka and kafka-python modules are installed, both should be uninstalled. After it to install kafka-python module only.
 from kafka import KafkaConsumer
+#PySocks module should be installed
+import socks
+import socket
+#import os
+#os.environ['NO_PROXY'] = 'socks5://localhost:1080'
 
+def bind_proxy_port(enable=True):
+    try:
+        if enable == True:
+            socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, 'localhost', 1080)
+            socket.socket = socks.socksocket
+            print('Proxy is enabled')
+        return True
+    except:
+        raise Exception("unable to set proxy 127.0.0.1:1080")
+
+bind_proxy_port()
 consumer = KafkaConsumer(
     'gcp.orders.fct.btcusd.0',
     bootstrap_servers=['procamp-cluster-m:9092'],
